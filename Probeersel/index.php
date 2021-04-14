e1b0b1056c9246182888f67875013426dfdeac6c
Rowan Norder <55745826+RowanNorder@users.noreply.github.com>
2021-04-14 12:58:15 +0200

    A

---
+++ Probeersel/index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Rowan Norder Portfolio Jaar 1</title>
+</head>
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero"><h1><span>R</span>owan <span>N</span>order</h1></a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger"><div class="bar"></div></div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#projects" data-after="Projects">Projecten</a></li>
+            <li><a href="#about" data-after="About">Over mij</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Hallo, <span></span></h1>
+        <h1>Mijn Naam is <span></span></h1>
+        <h1>Rowan Norder <span></span></h1>
+        <a href="#projects" type="button" class="cta">Portfolio</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  <!-- Projects Section -->
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Recente <span>Projecten</span></h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 1</h1>
+            <h2>Teamwebsite bouwen</h2>
+            <p>Het eerste project. hiervoor heb ik nog niet vaker iets in zon groepsverband gedaan dus dat was best nieuw.
+              Voor dit project moesten we een teamwebsite maken. dit deden we met HTML, CSS en PhP. 
+              Ik maakte in deze periode voor het eerst kennis met PHP en Github</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 2</h1>
+            <h2>TicTacToe en CMS</h2>
+            <p>Het tweede project. Dit was een solo project, wat ik fijn vond, waarbij we met C# het spel TICTACTOE
+              moesten maken. Dit vond ik ook heel leuk omdat je hier zonder code al beter kon zien wat het eindresultaat
+              zou gaan worden. En na het coderen te zien dat het werkt is erg leuk.</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 3</h1>
+            <h2>Gamehub met 3 Games</h2>
+            <p>Het derde project. Dit was een groepsproject. We werkten in duos om samen tot 1 hub met 3 games te komen.
+              De samenwerking tussen mij en mijn duo-partner verliep goed. En wij hebben de game MEAP gemaakt, dat is 
+              een soort van pong. </p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 4</h1>
+            <h2>Kassa,Keuken narrowcasting</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum enim libero dolores. Totam quidem optio distinctio, inventore consectetur quia commodi praesentium voluptas minima, eveniet id quibusdam adipisci accusamus, fugit fugiat.</p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 5</h1>
+            <h2>Just in Time en Mobile App</h2>
+            <p>Het derde project. Dit was een groepsproject. We werkten in duos om samen tot 1 hub met 3 games te komen.
+              De samenwerking tussen mij en mijn duo-partner verliep goed. En wij hebben de game MEAP gemaakt, dat is 
+              een soort van pong. </p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Project 6</h1>
+            <h2>Nog Niet Begonnen</h2>
+            <p>Het derde project. Dit was een groepsproject. We werkten in duos om samen tot 1 hub met 3 games te komen.
+              De samenwerking tussen mij en mijn duo-partner verliep goed. En wij hebben de game MEAP gemaakt, dat is 
+              een soort van pong. </p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="./img/img-2.png" alt="img">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">Over <span>mij</span></h1>
+        <h2>Student Applicatie- en mediaontwikkelaar</h2>
+        <p>Hallo, mijn naam is Rowan Mark Norder. 
+          Ik ben 18 jaar oud en woon in Franeker.
+          Ik ben tweede jaars MBO student op de opleiding 
+          applicatie- en mediaontwikkelaar.
+          Het coderen van websites vind ik momenteel het leukst.
+          Ook het designen vind ik leuk, vooral met HTML en CSS.
+          </p>
+
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
+
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div><h1 class="section-title">Contact <span>info</span></h1></div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png"/></div>
+          <div class="contact-info">
+            <h1>Telefoon</h1>
+            <h2>06-43231629</h2>
+            <h2>06-24469504</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png"/></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>rmnorder@gmail.com</h2>
+            <h2>rowan.mark.9@gmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png"/></div>
+          <div class="contact-info">
+            <h1>Adres</h1>
+            <h2>Franeker, Friesland, Nederland</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand"><h1><span>R</span>owan <span>N</span>order</h1></div>
+      <h2>Student Applicatie- en mediaontwikkelaar</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="https://www.facebook.com/profile.php?id=100009637805483"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png"/></a>
+        </div>
+        <div class="social-item">
+          <a href="https://www.instagram.com/rowannorder/"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png"/></a>
+        </div>
+        <div class="social-item">
+          <a href="https://twitter.com/rowan_norder"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png"/></a>
+        </div>
+      </div>
+      <p>Copyright © 2021 Rowan Norder. All rights reserved.</p>
+    </div>
+  </section>
+  <!-- End Footer -->
+  <script src="./app.js"></script>
+</body>
+</html>
